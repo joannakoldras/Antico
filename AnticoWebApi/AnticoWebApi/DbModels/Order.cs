a420d712f29b1b00d5e00af450a50bf94acfa40b
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-04-16 19:47:50 +0200

    Add Order Model

---
+++ AnticoWebApi/AnticoWebApi/DbModels/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace AnticoWebApi.DbModels
+{
+    public class Order
+    {
+        public int Id { get; set; } 
+        public string LongTextId { get; set; } //id zam
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}

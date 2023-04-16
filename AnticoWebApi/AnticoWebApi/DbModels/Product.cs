cb8a6f274972cddbbdb87654da799f5d07ac7eaa
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-04-16 19:42:32 +0200

    Add Product Model

---
+++ AnticoWebApi/AnticoWebApi/DbModels/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace AnticoWebApi.DbModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsAvaliable { get; set; }
+        public string FilePathPhoto { get; set; }
+        public double Price { get; set; }  
+    }
+}

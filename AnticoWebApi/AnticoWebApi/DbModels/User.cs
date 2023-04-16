82a6593ed1039e5ed5b2696e8247d434204c607c
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-04-16 19:34:31 +0200

    Add User Model

---
+++ AnticoWebApi/AnticoWebApi/DbModels/User.cs
@@ -0,0 +1,12 @@
+﻿namespace AnticoWebApi.DbModels
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string EncryptedPassword { get; set; } 
+        public string UserName { get; set; }
+    }
+}

369ff2216ca37c893a0db995e48dca562cc10a11
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-05-03 17:53:23 +0200

    Add GetAllProducts method template

---
+++ AnticoWebApi/AnticoWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using AnticoWebApi.DbModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnticoWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet(Name = "Products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return new List <Product>()
+            {
+
+            }; 
+        }
+    }
+}

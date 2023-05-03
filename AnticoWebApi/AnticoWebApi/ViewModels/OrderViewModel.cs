665758c2b57ae35ced6e9c8c2025d5b72afbf7ec
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-05-03 17:56:15 +0200

    Add ViewModels for Order, Product and User

---
+++ AnticoWebApi/AnticoWebApi/ViewModels/OrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AnticoWebApi.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string OrderId { get; set; } 
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ AnticoWebApi/AnticoWebApi/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AnticoWebApi.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public string Category { get; set; }  
+        public bool IsAvaliable { get; set; }
+        public string FilePathPhoto { get; set; }
+        public double Price { get; set; } 
+    }
+}

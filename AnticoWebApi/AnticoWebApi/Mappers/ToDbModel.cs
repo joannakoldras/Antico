c39a3333db63b6a88d9cc4c7afe9c0b261b8b86b
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-05-17 19:38:01 +0200

    Implement methods ToUserDbModel, ToOrderDbModel, ToProductDbModel

--- AnticoWebApi/AnticoWebApi/Mappers/ToDbModel.cs
+++ AnticoWebApi/AnticoWebApi/Mappers/ToDbModel.cs
@@ -1,6 +1,43 @@
-﻿namespace AnticoWebApi.Mappers
+﻿using AnticoWebApi.DbModels;
+using AnticoWebApi.ViewModels;
+
+namespace AnticoWebApi.Mappers
 {
-    public class ToDbModel
+    public static class ToDbModel
     {
+        public static Product ToProductDbModel(this ProductViewModel productViewModel)
+        {
+            var productDB = new Product(); 
+            productDB.Name = productViewModel.Name;
+            productDB.Description = productViewModel.Description;
+            //brak mechanizmu do konwersji enum na int
+            productDB.IsAvaliable = productViewModel.IsAvaliable;
+            productDB.FilePathPhoto = productViewModel.FilePathPhoto;
+            productDB.Price = productViewModel.Price; 
+
+            return productDB; 
+        }
+
+        public static Order ToOrderDbModel(this OrderViewModel orderViewModel)
+        {
+            var orderDb = new Order();
+            orderDb.LongTextId = orderViewModel.OrderId;
+            orderDb.UserId = orderViewModel.UserId;
+            orderDb.ProductId = orderViewModel.ProductId;
+
+            return orderDb;
+        }
+
+        public static User ToUserDbModel(this UserViewModel user)
+        {
+            var userDb = new User();
+            userDb.Name = user.Name;
+            userDb.Surname = user.Surname;
+            userDb.Email = user.Email;
+            //userVM.Password = user.; mechanizm hashowania hasla
+            userDb.UserName = user.UserName;
+
+            return userDb;
+        }
     }
 }

eb18612a183333c32432ca06bd9d0e8f26e75fac
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-05-17 21:35:47 +0200

    Testing Db connection

--- AnticoWebApi/AnticoWebApi/DbConnection/AnticoDbContext.cs
+++ AnticoWebApi/AnticoWebApi/DbConnection/AnticoDbContext.cs
@@ -10,6 +10,17 @@ namespace AnticoWebApi.DbConnection
         {
             
         }
+
+        public AnticoDbContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\\MSSQLLocalDB;database=AnticoDatabase;Trusted_Connection=True;");
+        }
+
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }

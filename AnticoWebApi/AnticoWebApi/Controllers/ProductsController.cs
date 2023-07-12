093fd51e78c2520ae84f0395b5dad6cd67cdedca
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-07-12 20:07:34 +0200

    add GetProductsByCategory endpoint with implementation

--- AnticoWebApi/AnticoWebApi/Controllers/ProductsController.cs
+++ AnticoWebApi/AnticoWebApi/Controllers/ProductsController.cs
@@ -10,21 +10,27 @@ namespace AnticoWebApi.Controllers
     public class ProductsController : ControllerBase
     {
         private IProductFinderService _productFinderService { get; set; }
-        public ProductsController() 
-        { 
+        public ProductsController()
+        {
             _productFinderService = new ProductFinderService();
         }
 
         [HttpGet(Name = "Products")]
         public IEnumerable<ProductViewModel> GetAllProducts()
         {
-            return _productFinderService.GetAllProducts(); 
+            return _productFinderService.GetAllProducts();
         }
 
         [HttpGet("{searchString}")]
         public IEnumerable<ProductViewModel> FindProducts(string searchString)
         {
             return _productFinderService.FindProducts(searchString);
+        }
+
+        [HttpGet("Categories/{category}")]
+        public IEnumerable<ProductViewModel> GetProductsByCategory(string category)
+        {
+            return _productFinderService.GetProductsByCategory(category); 
         }
     }
 }

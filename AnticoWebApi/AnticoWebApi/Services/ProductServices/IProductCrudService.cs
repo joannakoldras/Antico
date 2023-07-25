158a9fbc34fe2ea6c083348c57988e0bff20f810
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-07-25 21:08:16 +0200

    add IProductCrudService with implementation and ProductCrudService with implementation

---
+++ AnticoWebApi/AnticoWebApi/Services/ProductServices/IProductCrudService.cs
@@ -0,0 +1,11 @@
+﻿using AnticoWebApi.ViewModels;
+
+namespace AnticoWebApi.Services.ProductServices
+{
+    public interface IProductCrudService
+    {
+        bool AddProductToDb(ProductViewModel productVM);
+        bool UpdateProductInDb(ProductViewModel productVM);
+        bool DeleteProductFromDb(ProductViewModel productVM);
+    }
+}
---
+++ AnticoWebApi/AnticoWebApi/Services/ProductServices/ProductCrudService.cs
@@ -0,0 +1,36 @@
+﻿using AnticoWebApi.Mappers;
+using AnticoWebApi.Repository;
+using AnticoWebApi.ViewModels;
+
+namespace AnticoWebApi.Services.ProductServices
+{
+    public class ProductCrudService : IProductCrudService
+    {
+        private IProductRepository productRepository { get; set; }
+        public ProductCrudService()
+        {
+            productRepository = new ProductRepository();
+        }
+
+        public bool AddProductToDb(ProductViewModel productVM)
+        {
+            var product = productVM.ToProductDbModel();
+            var result = productRepository.AddProduct(product);
+            return result;
+        }
+
+        public bool UpdateProductInDb(ProductViewModel productVM)
+        {
+            var product = productVM.ToProductDbModel();
+            var result = productRepository.UpdateProduct(product);
+            return result;
+        }
+
+        public bool DeleteProductFromDb(ProductViewModel productVM)
+        {
+            var product = productVM.ToProductDbModel();
+            var result = productRepository.DeleteProduct(product);
+            return result;
+        }
+    }
+}

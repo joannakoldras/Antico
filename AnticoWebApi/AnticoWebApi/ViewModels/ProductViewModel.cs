30808534f08659b5247772b32bb57dc3c1b61ed3
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-07-25 21:12:22 +0200

    change Category property type from string to ProductCategoryEnum

--- AnticoWebApi/AnticoWebApi/ViewModels/ProductViewModel.cs
+++ AnticoWebApi/AnticoWebApi/ViewModels/ProductViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace AnticoWebApi.ViewModels
+﻿using AnticoWebApi.ViewModels.Enums;
+
+namespace AnticoWebApi.ViewModels
 {
     public class ProductViewModel
     {
+        public int Id { get; set; } 
         public string Name { get; set; } 
         public string Description { get; set; } 
-        public string Category { get; set; }  
+        public ProductCategoryEnum Category { get; set; }  
         public bool IsAvaliable { get; set; }
         public string FilePathPhoto { get; set; }
         public double Price { get; set; } 

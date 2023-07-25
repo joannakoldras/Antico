c5397fb9406310c35a4c202aacd842c965f80802
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-07-25 21:06:52 +0200

    add IProductRepository interface with implementation and ProductRepository class with implementation

---
+++ AnticoWebApi/AnticoWebApi/Repository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using AnticoWebApi.DbModels;
+
+namespace AnticoWebApi.Repository
+{
+    public interface IProductRepository
+    {
+        bool AddProduct(Product product);
+        bool UpdateProduct(Product product);
+        bool DeleteProduct(Product product);
+    }
+}
---
+++ AnticoWebApi/AnticoWebApi/Repository/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using AnticoWebApi.DbConnection;
+using AnticoWebApi.DbModels;
+
+namespace AnticoWebApi.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public bool AddProduct(Product product)
+        {
+            try
+            {
+                using (var db = new AnticoDbContext())
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges(); 
+                }
+                return true;
+            }
+            catch (Exception exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            try
+            {
+                using (var db = new AnticoDbContext())
+                {
+                    var item = db.Products.Where(x => x.Id == product.Id).FirstOrDefault();
+                    if (item != null)
+                    {
+                        item.Price = product.Price;
+                        item.FilePathPhoto = product.FilePathPhoto;
+                        item.Name = product.Name;
+                        item.Description = product.Description;
+                        item.CategoryId = product.CategoryId;   
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+            using (var db = new AnticoDbContext())
+            {
+                var item = db.Products.Where(x => x.Id == product.Id).FirstOrDefault();
+                if (item != null)
+                {
+                    db.Products.Remove(item);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

9f24deae8a0a2b5fa9136c824d9f70b3f5195275
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-07-25 21:11:16 +0200

    add ProductCategoryEnum

---
+++ AnticoWebApi/AnticoWebApi/ViewModels/Enums/ProductCategoryEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace AnticoWebApi.ViewModels.Enums
+{
+    public enum ProductCategoryEnum 
+    {
+        krzeslo = 1, 
+        mebel = 2, 
+        naczynia = 3,
+        //dodawac kategorie - ProductCategories db
+    }
+}

08fe90f88f9b2e086796e19e34a5619284314b2f
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-02 20:38:34 +0200

    Add DataResult class with implementation

---
+++ AnticoWebApi/AnticoWebApi/DataWrappers/DataResult.cs
@@ -0,0 +1,27 @@
+﻿namespace AnticoWebApi.DataWrappers
+{
+    public class DataResult
+    {
+        public bool success { get; set; }
+        public IEnumerable<object> data { get; set; }
+        public string message { get; set; } 
+        public DataResult(bool success, IEnumerable<object> data)
+        {
+            this.success = success;
+            this.data = data;
+        }
+
+        public DataResult(bool success, string message)
+        {
+            this.success = success;
+            this.message = message;  
+        }
+
+        public DataResult(bool success, IEnumerable<object> data, string message) 
+        {
+            this.message = message;
+            this.success = success;
+            this.data = data; 
+        }
+    }
+}

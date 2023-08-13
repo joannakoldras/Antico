34a82bb314f3dcd034f8148ea42467e7e77e7b2e
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-13 19:18:37 +0200

    change return datatype in AddProduct, UpdateProduct and DeleteProduct to DataResult

--- AnticoWebApi/AnticoWebApi/Repository/IProductRepository.cs
+++ AnticoWebApi/AnticoWebApi/Repository/IProductRepository.cs
@@ -1,11 +1,12 @@
-﻿using AnticoWebApi.DbModels;
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.DbModels;
 
 namespace AnticoWebApi.Repository
 {
     public interface IProductRepository
     {
-        bool AddProduct(Product product);
-        bool UpdateProduct(Product product);
-        bool DeleteProduct(Product product);
+        DataResult AddProduct(Product product);
+        DataResult UpdateProduct(Product product);
+        DataResult DeleteProduct(Product product);
     }
 }

66001726de13cfe2ec9b7113a82ffbc3532df122
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-13 21:06:23 +0200

    add interface IShoppingCartService with methods to implement

---
+++ AnticoWebApi/AnticoWebApi/Services/ShoppingCartServices/IShoppingCartService.cs
@@ -0,0 +1,12 @@
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.ViewModels;
+
+namespace AnticoWebApi.Services.ShoppingCartServices
+{
+    public interface IShoppingCartService
+    {
+        DataResult AddProductToShoppingCart(ProductViewModel productVM);
+        DataResult DeleteProductFromShoppingCart(ProductViewModel productVM);
+        DataResult GetAllProductsFromShoppingCart(); 
+    }
+}

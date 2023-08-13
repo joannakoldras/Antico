c34a8f5f965fd291a08e088b6b143d9b8ddf935f
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-13 21:05:33 +0200

    add ShoppingCart class with implementation

---
+++ AnticoWebApi/AnticoWebApi/DomainModels/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿using AnticoWebApi.DbModels;
+
+namespace AnticoWebApi.DomainModels
+{
+    public class ShoppingCart
+    {
+        private List<Product> products { get; set; } 
+        public ShoppingCart()
+        {
+            this.products = new List<Product>(); 
+        }
+
+        public void AddToShoppingCart(Product product)
+        {
+            products.Add(product); 
+        }
+
+        public bool DeleteFromShoppingCart(Product product)
+        {
+            var item = FindProductInShoppingCartById(product); 
+            if(item != null)
+            {
+                this.products.Remove(item);
+                return true;
+            }
+            return false; 
+        }
+
+        public Product FindProductInShoppingCartById(Product product)
+        {
+            var item = products.Find(x => x.Id == product.Id);
+            return item; 
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products; 
+        }
+    }
+}

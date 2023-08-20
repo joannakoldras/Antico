0ccc4751bc9328e1d69da8c0b252f37a510d42f5
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-20 20:04:16 +0200

    add ShoppingCartController with implementation

---
+++ AnticoWebApi/AnticoWebApi/Controllers/ShoppingCartController.cs
@@ -0,0 +1,38 @@
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.Services.ShoppingCartServices;
+using AnticoWebApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnticoWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private IShoppingCartService _shoppingCartService { get; set; }
+        
+        public ShoppingCartController()
+        {
+            _shoppingCartService = new ShoppingCartService(); 
+        }
+
+        [HttpPost("Products")]
+        public DataResult AddProductToShoppingCart(ProductViewModel product)
+        {
+            return _shoppingCartService.AddProductToShoppingCart(product);
+        }
+
+        [HttpGet("Products")]
+        public DataResult GetAllProductsFromShoppingCart()
+        {
+            return _shoppingCartService.GetAllProductsFromShoppingCart(); 
+        }
+
+        [HttpDelete("Products")]
+        public DataResult DeleteProductFromShoppingCart(ProductViewModel product)
+        {
+            return _shoppingCartService.DeleteProductFromShoppingCart(product); 
+        }
+    }
+}

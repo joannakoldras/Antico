1438cdf4f7ec4586b774670fcef2e5340de83a8d
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-20 21:17:56 +0200

    add UserController with register method

---
+++ AnticoWebApi/AnticoWebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.DbModels;
+using AnticoWebApi.Services.UserServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnticoWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IRegisterService _registerService { get; set; }
+
+        public UserController()
+        {
+            _registerService = new RegisterService();
+        }
+
+        [HttpPost("/Register")]
+        public DataResult RegisterUser(User user)
+        {
+            return _registerService.RegisterUser(user);
+        }
+    }
+}

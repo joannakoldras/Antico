08a7102b4961e0a56aeec504f5a31d4c538c7d69
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-20 21:20:26 +0200

    add IRegisterService with register method

---
+++ AnticoWebApi/AnticoWebApi/Services/UserServices/IRegisterService.cs
@@ -0,0 +1,10 @@
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.DbModels;
+
+namespace AnticoWebApi.Services.UserServices
+{
+    public interface IRegisterService
+    {
+        DataResult RegisterUser(User user); 
+    }
+}

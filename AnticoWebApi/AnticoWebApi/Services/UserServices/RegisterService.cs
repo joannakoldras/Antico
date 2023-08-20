6b198386dd57e0db5a922c0a78e6d1dbe3a6f39a
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-08-20 21:18:51 +0200

    add RegisterService with register method

---
+++ AnticoWebApi/AnticoWebApi/Services/UserServices/RegisterService.cs
@@ -0,0 +1,30 @@
+﻿using AnticoWebApi.DataWrappers;
+using AnticoWebApi.DbConnection;
+using AnticoWebApi.DbModels;
+
+namespace AnticoWebApi.Services.UserServices
+{
+    public class RegisterService : IRegisterService
+    {
+        public DataResult RegisterUser(User user)
+        //przemapować user na UserViewModel 
+        {
+            using (var dbContext = new AnticoDbContext())
+            {
+                var dbUser = dbContext.Users.
+                    Where(x => x.Email == user.Email &&
+                    x.UserName == user.UserName &&
+                    x.EncryptedPassword == user.EncryptedPassword).ToList();
+
+                if (dbUser.Count() == 0)
+                {
+                    dbContext.Add(user);
+                    dbContext.SaveChanges();
+                }
+                else return new DataResult(false, "User with passed data already exist");
+
+                return new DataResult(true, "User successfully registered"); 
+            }
+        }
+    }
+}

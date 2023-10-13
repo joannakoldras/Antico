62938eb1915ad32e30c6e76c36c8afadb7588cb8
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-10-13 21:59:53 +0200

    add Services for handling session

--- AnticoWebApi/AnticoWebApi/Program.cs
+++ AnticoWebApi/AnticoWebApi/Program.cs
@@ -1,5 +1,6 @@
 using AnticoWebApi.DbConnection;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,20 +10,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDistributedMemoryCache(); 
+builder.Services.AddSession(options => 
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(1800);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor(); 
 
 var app = builder.Build();
-
+app.UseSession();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage(); 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();

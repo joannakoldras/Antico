3ed4c085ac82eabacd417560ac6e114466aea549
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-10-19 22:04:14 +0200

    add GetAllItemFromSession method, add sessionItemCounter, add CountItemsInSession, add ClearSession, add DeleteItemFromSession

--- AnticoWebApi/AnticoWebApi/Session/SessionHelper.cs
+++ AnticoWebApi/AnticoWebApi/Session/SessionHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AnticoWebApi.DbModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
 using Newtonsoft.Json;
@@ -9,24 +10,77 @@ namespace AnticoWebApi.Session
     public class SessionHelper : ControllerBase 
     {
         private IHttpContextAccessor httpContextAccessor;
+        private int sessionItemCounter; 
         public SessionHelper()
         {
             httpContextAccessor = new HttpContextAccessor();
+            sessionItemCounter = 0;
+            CountItemsInSession(); 
         }
         public void SetSessionWithValue(object value)
         {
             SetObjectAsJson("1", value); 
         }
+
+        public IEnumerable<Product> GetAllItemsFromSession()
+        {
+            var items = new List<Product>(); 
+            for (int i=0; i<sessionItemCounter; i++)
+            {
+                var sessionItem = GetObjectFromJson<Product>(i.ToString()); 
+                items.Add(sessionItem); 
+            } 
+            return items;  
+        }
+
         private void SetObjectAsJson(string key, object value)
         {
-            httpContextAccessor.HttpContext.Session.SetString(JsonConvert.SerializeObject(value), JsonConvert.SerializeObject(value));
+            httpContextAccessor.HttpContext.Session.SetString(sessionItemCounter.ToString(), JsonConvert.SerializeObject(value));
         }
 
         private T GetObjectFromJson<T>(string key)
         {
             var value = httpContextAccessor.HttpContext.Session.GetString(key);
-            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value); 
 
         }
+
+        //do naprawy ponowne ustawianie obiektów w sesji
+        public bool DeleteItemFromSession(Product product)
+        {
+            var items = GetAllItemsFromSession().ToList();
+            var itemToDelete = items.Where(x => x.Id == product.Id).FirstOrDefault(); 
+
+            if(itemToDelete != null)
+            {
+                items.Remove(itemToDelete); 
+            }
+            else
+            {
+                return false; 
+            }
+
+            ClearSession(); 
+            var counter = 0; 
+            
+            foreach (var item in items)
+            {
+                SetObjectAsJson(counter.ToString(), item); 
+                counter++; 
+            }
+            return true; 
+        }
+
+        private void ClearSession()
+        {
+            httpContextAccessor.HttpContext.Session.Clear(); 
+        }
+
+        private void CountItemsInSession()
+        {
+            sessionItemCounter = httpContextAccessor.HttpContext.Session.Keys.Count(); 
+        }
+
+
     }
 }
